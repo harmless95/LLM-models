707287296dea6dd1cb71ff004a1606bf0938c1d2
Vitaly Zakharov <zaharof1995@yandex.ru>
2025-11-05 21:12:50 +0700

    create Accelerator.py

---
+++ my_project/Fine_tuning_models/Accelerator.py
@@ -0,0 +1,73 @@
+from accelerate import Accelerator
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    get_scheduler,
+)
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from tqdm.auto import tqdm
+
+checkpoint = "bert-base-uncased"
+
+# Строка инициализирует объект Accelerator, который проанализирует окружение и определит необходимые настройки
+accelerator = Accelerator()
+raw_datasets = load_dataset("glue", "mrpc")
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+optimizer = AdamW(model.parameters(), lr=5e-5)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+tokenized_datasets = tokenized_datasets.remove_columns(
+    ["sentence1", "sentence2", "idx"]
+)
+tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+tokenized_datasets.set_format("torch")
+
+train_dataloader = DataLoader(
+    tokenized_datasets["train"],
+    shuffle=True,
+    batch_size=8,
+    collate_fn=data_collator,
+)
+eval_dataloader = DataLoader(
+    tokenized_datasets["validation"],
+    batch_size=8,
+    collate_fn=data_collator,
+)
+
+# Этот метод «обернет» объекты в контейнер и убедится, что распределенное обучение выполняется корректно.
+train_dl, eval_dl, model, optimizer = accelerator.prepare(
+    train_dataloader,
+    eval_dataloader,
+    model,
+    optimizer,
+)
+num_epochs = 3
+num_training_steps = num_epochs * len(train_dl)
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+progress_bar = tqdm(range(num_training_steps))
+model.train()
+for epoch in range(num_epochs):
+    for batch in train_dl:
+        outputs = model(**batch)
+        loss = outputs.loss
+        accelerator.backward(loss=loss)
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
